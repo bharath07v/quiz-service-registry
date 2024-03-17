5ab655189cf329d4722290942cf88e248e0ac15d
bharath07v <slnbd7@gmail.com>
2024-03-17 13:51:25 +0530

    initial commit

---
+++ src/test/java/com/example/quizserviceregistry/QuizServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
